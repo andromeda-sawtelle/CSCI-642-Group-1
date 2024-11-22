9c4221f6b8ad02d68f4758603acf1d090ff4973f
Andromeda Sawtelle <andromeda.sawtelle@gmail.com>
2024-11-22 11:24:15 -0500

    Updated projreport to talk about asan parse level

--- projreport/asanpoison.c
+++ projreport/asanpoison.c
@@ -1,6 +1,6 @@
 char* identGlobal = NULL;
-char* poisonedIdents[1024];
-int poisonedIndex = 0;
+char* poisonedIdents[32][1024];
+int poisonedIndex[32];
 char prevUnaryStar = 0;
 
 ...
@@ -9,20 +9,19 @@ char toPoison = 0;
 if(identGlobal != NULL && strcmp(identGlobal, "free") == 0) {
   toPoison = 1;
 }
-
 // DONE: support __func__
 char* ident = IdentTable + s;
 if(toPoison) {
-  poisonedIdents[poisonedIndex] = ident;
-  poisonedIndex++;
+  poisonedIdents[ParseLevel][poisonedIndex[ParseLevel]] = ident;
+  poisonedIndex[ParseLevel]++;
 }
-
 if(prevUnaryStar) {
-  for(int i = 0; i < poisonedIndex; i++) {
-    if(strcmp(ident, poisonedIdents[i]) == 0) {
-      error("ident: %s is poisoned\n", ident);
+  for(int j = ParseLevel; j >= 0; j--) {
+    for(int i = 0; i < poisonedIndex[ParseLevel]; i++) {
+      if(strcmp(ident, poisonedIdents[ParseLevel][i]) == 0) {
+        error("ident: %s is poisoned\n", ident);
+      }
     }
   }
 }
-
 identGlobal = ident;
